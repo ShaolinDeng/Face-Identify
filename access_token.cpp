2a5d161809314e28665208ae86e149169bc01344
Mauriyin <mauri@foxmail.com>
2017-08-15 21:09:00 +0800

    change access

--- access_token.cpp
+++ access_token.cpp
@@ -2,9 +2,7 @@
 #include <curl/curl.h>
 #include <json.h>
 #include "access_token.h"
-// libcurl库下载链接：https://curl.haxx.se/download.html
-// jsoncpp库下载链接：https://github.com/open-source-parsers/jsoncpp/
-// 获取access_token所需要的url
+//token url
 const std::string access_token_url = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials";
 // 回调函数获取到的access_token存放变量
 // static std::string access_token_result;
@@ -63,49 +61,3 @@ int get_access_token(std::string &access_token, const std::string &AK, const std
 	return error_code;
 }
 
-errno_t fileBase64Encode(const char *srcFile, const char *dstFile)
-{
-	FILE *fr = NULL, *fw = NULL;
-	errno_t err = 0;
-
-	if (err == 0)
-	{
-		err = fopen_s(&fr, srcFile, "rb");
-		if (err != 0)
-		{
-			printf("The file '%s' can't be open\n", srcFile);
-		}
-	}
-
-	if (err == 0)
-	{
-		err = fopen_s(&fw, dstFile, "wb+");
-		if (err != 0)
-		{
-			printf("The file '%s' can't be open,%d\n", dstFile, err);
-		}
-	}
-
-	if (err == 0)
-	{
-		const int tLen = ATLSMTP_MAX_BASE64_LINE_LENGTH; // = 57  
-		BYTE srcBuf[tLen + 1];
-		char dstBuf[tLen * 4 / 3 + 5]; //+\r\n\r\n\0 //  
-		size_t nTotal = 0;
-		while (!feof(fr))
-		{
-			int nLen = fread(srcBuf, 1, tLen, fr);
-			int i = Base64EncodeGetRequiredLength(nLen);
-			Base64Encode(srcBuf, nLen, dstBuf, &i);
-			nTotal += fwrite(dstBuf, 1, i, fw);
-
-		}
-	}
-
-	if (fr)
-		fclose(fr);
-	if (fw)
-		fclose(fw);
-
-	return err;
-}
